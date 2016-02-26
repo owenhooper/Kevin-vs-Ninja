5b6eda2f2222945d169dc3140fcc0773a18a3d87
Owen Hooper <Owen@Owens-MacBook-Pro.local>
2016-02-25 21:08:29 -0800

    enter

---
+++ three-punch-fight/BrownUpImg.swift
@@ -0,0 +1,9 @@
+//
+//  BrownUpImg.swift
+//  three-punch-fight
+//
+//  Created by Owen Hooper on 2016-02-25.
+//  Copyright © 2016 Owen Hooper. All rights reserved.
+//
+
+import Foundation
---
+++ three-punch-fight/Stamina.swift
@@ -0,0 +1,9 @@
+//
+//  Stamina.swift
+//  three-punch-fight
+//
+//  Created by Owen Hooper on 2016-02-24.
+//  Copyright © 2016 Owen Hooper. All rights reserved.
+//
+
+import Foundation
